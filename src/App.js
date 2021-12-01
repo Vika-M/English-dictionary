f19a1466ecdf79b45c033fde4615bb694eca13bc
Viktoria <Vikulya_1803@mail.ru>
2021-12-01 11:13:24 +0300

    in work

--- src/App.js
+++ src/App.js
@@ -4,25 +4,48 @@ import Card from './components/card/card';
 import Table from './components/table/table';
 import words from './components/assets/Words';
 import Header from './components/header/header';
-import ButtonUp from './components/buttonUp/ButtonUp'
-import ButtonDown from './components/buttonDown/ButtonDown';
+import { useState } from 'react';
+
 
 
 function App() {
+ const [indexArray, setindexArray] = useState(0);
+
+const sliderRight = () => {
+  if (indexArray < words.length -1 ) {
+    setindexArray(indexArray+1)
+} 
+};
+const sliderLeft = () => {
+   if (indexArray > 0 ) {
+    setindexArray(indexArray-1)
+  }
+     
+} ;
+
+
   return (
     <div className="App"> 
     <Header></Header>
     <div  className="App1" >
       
-     <ButtonDown></ButtonDown>
-    {
-      words.map (  (word) =>
-      <Card english={word.english}  transcription={word.transcription} russian={word.russian} tags={word.tags} > </Card>
-        )
-      
-    }
-  <ButtonUp></ButtonUp>
+  
+    <div>
+        <button className="btnDown"  onClick={sliderLeft}  >Назад</button>
+    </div>
+        
+      <Card english={words[indexArray].english}  transcription={words[indexArray].transcription} russian={words[indexArray].russian} tags={words[indexArray].tags} > </Card>
+        
+      <div>
+         <button className="btnUp" onClick={sliderRight} >Вперед</button>
+       </div>
+
+       
+    
+
+  
    </div>
+   <div>{indexArray+1 + '/' + words.length}</div>
    <div  className="App2" >
     <Table></Table>
     </div>
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import './_card.scss'
+import './_card.scss';
+import '../assets/Words';
 
 
 
@@ -9,15 +10,21 @@ function Card(props) {
     const handleChange = () => {
         setPressed(!pressed);
     }
+
+
     const { english, transcription, russian, tags } = props;
     return (<React.Fragment>
 
+
+
         <div div className="cardWrapper" >
+
             <div className="cardEnglish">{english}</div>
             <div className="cardTranscription">{transcription}</div>
 
             <div className="cardTags">{tags}</div>
             <button onClick={handleChange} className="cardButton">{pressed ? [russian] : 'Проверить'}</button>
+
         </div >
 
     </React.Fragment>
